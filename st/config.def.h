cac0a5254697669c91a93acc43ea2d687587d882
Caden Henrich <cadenhenrich@gmail.com>
2020-11-04 20:44:19 -0500

    Added new st + waloop

--- st/config.def.h
+++ st/config.def.h
@@ -106,6 +106,9 @@ char *termname = "st-256color";
  */
 unsigned int tabspaces = 4;
 
+/* bg opacity */
+float alpha = 0.9;
+
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 
--- st/st.h
+++ st/st.h
@@ -137,4 +137,5 @@ extern char *termname;
 extern unsigned int tabspaces;
 extern unsigned int defaultfg;
 extern unsigned int defaultbg;
+extern float alpha;
 extern const int boxdraw, boxdraw_bold, boxdraw_braille;
